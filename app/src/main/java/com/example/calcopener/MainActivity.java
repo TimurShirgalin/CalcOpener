5b0d8d163a12ab853a5b38f5b4306c51fc843c04
Timur <shirgalin@hotmail.com>
2021-01-23 23:38:59 +0300

    Lesson5

--- app/src/main/java/com/example/calcopener/MainActivity.java
+++ app/src/main/java/com/example/calcopener/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.calcopener;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.Button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("calc://open");
+                Intent calcOpener = new Intent(Intent.ACTION_VIEW, uri);
+                ActivityInfo activityInfo = calcOpener.resolveActivityInfo(getPackageManager(), calcOpener.getFlags());
+                if (activityInfo != null) {
+                    startActivity(calcOpener);
+                }
+            }
+        });
     }
 }
